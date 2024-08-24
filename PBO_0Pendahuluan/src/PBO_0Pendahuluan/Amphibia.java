e4158db22024edcd5e3a35999b10514c48e50cd4
UmarAziz01 <147779314+UmarAziz01@users.noreply.github.com>
2024-08-24 13:44:48 +0700

    Add files via upload

---
+++ PBO_0Pendahuluan/src/PBO_0Pendahuluan/Amphibia.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PBO_0Pendahuluan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Amphibia extends Hewan {
+
+    public void utama() {
+        System.out.println("Ciri utama yaitu mengalami metamorfosis lengkap yang hanya ada di vertebrata");
+    }
+
+    public void jantung() {
+        System.out.println("Memiliki jantung yang terdiri dari tiga ruang, satu ventrikel dan dua buah atrium");
+    }
+
+    public void reproduksi() {
+        System.out.println("Akan bereproduksi dengan bertelur serta pembuahan eksternal");
+    }
+
+    public void kulit() {
+        System.out.println("Memiliki kulit yang cenderung berlendir");
+    }
+
+}
---
+++ PBO_0Pendahuluan/src/PBO_0Pendahuluan/Mamalia.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PBO_0Pendahuluan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mamalia extends Hewan {
+
+    public void utama() {
+        System.out.println("Ciri utamanya memiliki kelenjar susu");
+    }
+
+    public void rambut() {
+        System.out.println("Umummnya memiliki rambut ditubuhnya");
+    }
+
+    public void tulang() {
+        System.out.println("Dia termasuk vertebrata yang menjadikan dia memilki tulang belakang");
+    }
+
+    public void jantung() {
+        System.out.println("Memiliki jantung yang terdiri dari 4 ruang, dua ventrikel dan dua buah atrium. Sistem peredaran darahnya tertutup dan ganda");
+    }
+}
